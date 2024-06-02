5b3009d07e91d50eb2e958806cffd8357b3762a3
joaov1ct0r <jov9051@outlook.com>
2024-06-02 03:33:18 -0300

    test:

---
+++ src/test/java/com/joaov1ct0r/restful_api_users_java/RestfulApiUsersJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaov1ct0r.restful_api_users_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiUsersJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
