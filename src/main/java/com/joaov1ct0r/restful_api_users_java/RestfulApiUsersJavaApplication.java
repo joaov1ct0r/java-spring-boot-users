b613e156044c798a7cdbfc53e53c78b81883d2ef
joaov1ct0r <jov9051@outlook.com>
2024-06-02 03:34:01 -0300

    feat: main

---
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/RestfulApiUsersJavaApplication.java
@@ -0,0 +1,13 @@
+package com.joaov1ct0r.restful_api_users_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiUsersJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiUsersJavaApplication.class, args);
+	}
+
+}
