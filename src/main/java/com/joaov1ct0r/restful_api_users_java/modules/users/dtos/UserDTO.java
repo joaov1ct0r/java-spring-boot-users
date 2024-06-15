f5e938bfea8bb78d7686a42040908093bb0240e9
joaov1ct0r <jov9051@outlook.com>
2024-06-15 03:00:44 -0300

    feat: user dto

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/dtos/UserDTO.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/dtos/UserDTO.java
@@ -1,2 +1,70 @@
-package com.joaov1ct0r.restful_api_users_java.modules.users.dtos;public class UserDTO {
+package com.joaov1ct0r.restful_api_users_java.modules.users.dtos;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class UserDTO {
+    public UUID id;
+    public String username;
+    public String name;
+    public String email;
+    public LocalDateTime createdAt;
+    public LocalDateTime updatedAt;
+    public UUID userWhoUpdatedId;
+
+    public UUID getUserWhoUpdatedId() {
+        return userWhoUpdatedId;
+    }
+
+    public void setUserWhoUpdatedId(UUID userWhoUpdatedId) {
+        this.userWhoUpdatedId = userWhoUpdatedId;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 }
