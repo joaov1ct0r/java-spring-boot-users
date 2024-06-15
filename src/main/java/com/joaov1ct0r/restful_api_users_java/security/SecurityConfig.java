d1ad2d62f00adc65d5e815ee4a9783d37d53c9d0
joaov1ct0r <jov9051@outlook.com>
2024-06-15 03:04:06 -0300

    feat: security config

--- src/main/java/com/joaov1ct0r/restful_api_users_java/security/SecurityConfig.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/security/SecurityConfig.java
@@ -1,4 +1,39 @@
 package com.joaov1ct0r.restful_api_users_java.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+    private static final String[] PERMIT_ALL_LIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resource/**",
+            "/actuator/**"
+    };
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/signin/").permitAll()
+                            .requestMatchers("/signup/").permitAll()
+                            .requestMatchers(PERMIT_ALL_LIST).permitAll();
+
+                    auth.anyRequest().authenticated();
+                });
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
