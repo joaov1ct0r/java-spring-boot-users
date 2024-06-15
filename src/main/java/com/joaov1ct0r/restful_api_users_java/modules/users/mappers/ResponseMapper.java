e16f969976df067657dfbc8cf8b0c8dbe97622e1
joaov1ct0r <jov9051@outlook.com>
2024-06-15 03:02:51 -0300

    feat: response mapper

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/mappers/ResponseMapper.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/mappers/ResponseMapper.java
@@ -1,2 +1,19 @@
-package com.joaov1ct0r.restful_api_users_java.modules.users.mappers;public class ResponseMapper {
+package com.joaov1ct0r.restful_api_users_java.modules.users.mappers;
+
+import com.joaov1ct0r.restful_api_users_java.modules.users.dtos.ResponseDTO;
+import jakarta.annotation.Nullable;
+
+public class ResponseMapper {
+    public static ResponseDTO toDTO(
+            Number statusCode,
+            String message,
+            @Nullable Object resource
+            ) {
+
+        return new ResponseDTO(
+               statusCode,
+               message,
+               resource
+       );
+    }
 }
