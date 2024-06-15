cefe2bc33b47fdf1b26535ee77588a9b51e3d8b6
joaov1ct0r <jov9051@outlook.com>
2024-06-15 03:12:14 -0300

    feat: TestUtils

--- src/test/java/com/joaov1ct0r/restful_api_users_java/modules/users/utils/TestUtils.java
+++ src/test/java/com/joaov1ct0r/restful_api_users_java/modules/users/utils/TestUtils.java
@@ -1,2 +1,16 @@
-package com.joaov1ct0r.restful_api_users_java.modules.users.utils;public class TestUtils {
+package com.joaov1ct0r.restful_api_users_java.modules.users.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class TestUtils {
+    public static String objectToJson(Object obj) {
+        try {
+            final ObjectMapper objMapper = new ObjectMapper();
+            objMapper.registerModule(new JavaTimeModule());
+            return objMapper.writeValueAsString(obj);
+        } catch(Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
