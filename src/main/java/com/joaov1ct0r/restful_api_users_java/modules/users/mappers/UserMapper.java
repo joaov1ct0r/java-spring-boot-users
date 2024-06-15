5c6fba1bbc138b359daf7a2d41a3bfd32042056c
joaov1ct0r <jov9051@outlook.com>
2024-06-15 03:03:11 -0300

    feat: user mapper

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/mappers/UserMapper.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/mappers/UserMapper.java
@@ -1,2 +1,18 @@
-package com.joaov1ct0r.restful_api_users_java.modules.users.mappers;public class UserMapper {
+package com.joaov1ct0r.restful_api_users_java.modules.users.mappers;
+
+import com.joaov1ct0r.restful_api_users_java.modules.users.dtos.UserDTO;
+import com.joaov1ct0r.restful_api_users_java.modules.users.entities.UserEntity;
+
+public class UserMapper {
+    public static UserDTO toDTO(UserEntity user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setName(user.getName());
+        userDTO.setCreatedAt(user.getCreatedAt());
+        userDTO.setUpdatedAt(user.getUpdatedAt());
+        userDTO.setUserWhoUpdatedId(user.getUserWhoUpdatedId());
+        return userDTO;
+    }
 }
