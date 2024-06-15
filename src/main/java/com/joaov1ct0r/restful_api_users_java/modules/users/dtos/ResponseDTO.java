7a4138ce4502acafec42d3330a3d64547f038a8c
joaov1ct0r <jov9051@outlook.com>
2024-06-15 03:00:24 -0300

    feat: responseDTO

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/dtos/ResponseDTO.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/dtos/ResponseDTO.java
@@ -1,2 +1,47 @@
-package com.joaov1ct0r.restful_api_users_java.modules.users.dtos;public class ResponseDTO {
+package com.joaov1ct0r.restful_api_users_java.modules.users.dtos;
+
+import jakarta.annotation.Nullable;
+
+public class ResponseDTO {
+    public Number statusCode;
+    public String message;
+    @Nullable
+    public Object resource;
+
+    public Number getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Number statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Nullable
+    public Object getResource() {
+        return resource;
+    }
+
+    public void setResource(@Nullable Object resource) {
+        this.resource = resource;
+    }
+
+    public ResponseDTO() {}
+
+    public ResponseDTO(
+            Number statusCode,
+            String message,
+            @Nullable Object resource
+    ) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.resource = resource;
+    }
 }
