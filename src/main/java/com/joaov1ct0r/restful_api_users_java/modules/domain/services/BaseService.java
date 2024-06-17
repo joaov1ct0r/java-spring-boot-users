2ca1f9f7ba3bf18f75f0af15608971a754a2c898
joaov1ct0r <jov9051@outlook.com>
2024-06-16 22:07:14 -0300

    feat: BaseService

---
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/domain/services/BaseService.java
@@ -0,0 +1,30 @@
+package com.joaov1ct0r.restful_api_users_java.modules.domain.services;
+
+import com.joaov1ct0r.restful_api_users_java.modules.domain.entities.ErrorLogEntity;
+import com.joaov1ct0r.restful_api_users_java.modules.domain.repositories.ErrorLogsRepository;
+import jakarta.annotation.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public abstract class BaseService {
+    @Autowired
+    protected ErrorLogsRepository errorLogsRepository;
+
+    protected void generateErrorLog(
+            @Nullable UUID userId,
+            Number code,
+            String description
+    ) {
+        this.errorLogsRepository.save(
+                new ErrorLogEntity(
+                        UUID.randomUUID(),
+                        userId,
+                        LocalDateTime.now(),
+                        code,
+                        description
+                )
+        );
+    }
+}
