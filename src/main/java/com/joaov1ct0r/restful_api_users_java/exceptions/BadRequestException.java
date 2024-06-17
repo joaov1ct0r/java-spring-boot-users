43dab6a40fd9984aa7e316870224e6085f1b5364
joaov1ct0r <jov9051@outlook.com>
2024-06-16 21:56:54 -0300

    feat: bad request exception

--- src/main/java/com/joaov1ct0r/restful_api_users_java/exceptions/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.joaov1ct0r.restful_api_users_java.exceptions;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
