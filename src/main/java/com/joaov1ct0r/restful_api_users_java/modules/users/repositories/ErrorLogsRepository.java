a1b3b6f247e4dca8c9fe165c50be59f0615eeae2
joaov1ct0r <jov9051@outlook.com>
2024-06-16 22:02:38 -0300

    feat: error logs repository

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/repositories/ErrorLogsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.joaov1ct0r.restful_api_users_java.modules.users.repositories;
-
-import com.joaov1ct0r.restful_api_users_java.modules.users.entities.ErrorLogEntity;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface ErrorLogsRepository extends JpaRepository<ErrorLogEntity, UUID> {
-}
