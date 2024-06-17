e3bf48e2a911e868a755e98bfff02b4a6d8a9cf8
joaov1ct0r <jov9051@outlook.com>
2024-06-16 22:01:08 -0300

    feat: CreateUserController

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/controllers/CreateUserController.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/controllers/CreateUserController.java
@@ -1,12 +1,12 @@
 package com.joaov1ct0r.restful_api_users_java.modules.users.controllers;
 
-import com.joaov1ct0r.restful_api_users_java.exceptions.BadRequestException;
-import com.joaov1ct0r.restful_api_users_java.modules.users.dtos.ResponseDTO;
+import com.joaov1ct0r.restful_api_users_java.modules.domain.exceptions.BadRequestException;
+import com.joaov1ct0r.restful_api_users_java.modules.domain.controllers.BaseController;
+import com.joaov1ct0r.restful_api_users_java.modules.domain.dtos.ResponseDTO;
+import com.joaov1ct0r.restful_api_users_java.modules.users.dtos.CreateUserDTO;
 import com.joaov1ct0r.restful_api_users_java.modules.users.dtos.UserDTO;
-import com.joaov1ct0r.restful_api_users_java.modules.users.entities.EventLogEntity;
 import com.joaov1ct0r.restful_api_users_java.modules.users.entities.UserEntity;
-import com.joaov1ct0r.restful_api_users_java.modules.users.mappers.ResponseMapper;
-import com.joaov1ct0r.restful_api_users_java.modules.users.repositories.EventLogsRepository;
+import com.joaov1ct0r.restful_api_users_java.modules.domain.repositories.EventLogsRepository;
 import com.joaov1ct0r.restful_api_users_java.modules.users.services.CreateUserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/signup/")
-public class CreateUserController {
+public class CreateUserController extends BaseController {
+
     @Autowired
     private CreateUserService createUserService;
 
@@ -28,31 +27,27 @@ public class CreateUserController {
     private EventLogsRepository eventLogsRepository;
 
     @PostMapping("/")
-    public ResponseEntity<Object> handle(@Valid @RequestBody UserEntity user) {
+    public ResponseEntity<Object> handle(@Valid @RequestBody CreateUserDTO user) {
         try {
             UserDTO createdUser = this.createUserService.execute(user);
 
-            ResponseDTO response = ResponseMapper.toDTO(
+            ResponseDTO response = this.responseMapper.toDTO(
                     201,
                     "Usuário criado com sucesso!",
                     createdUser
             );
 
-            this.eventLogsRepository.save(
-                    new EventLogEntity(
-                            UUID.randomUUID(),
-                            createdUser.getId(),
-                            LocalDateTime.now(),
-                            201,
-                            "Usuário com id:" + createdUser.getId() + " criado com sucesso!"
-                    )
+            this.generateEventLog(
+                    createdUser.getId(),
+                    201,
+                    "Usuário com id:" + createdUser.getId() + " criado com sucesso!"
             );
 
             return ResponseEntity.status(201).body(response);
         }
         catch(Exception e) {
             if (e instanceof BadRequestException) {
-                ResponseDTO response = ResponseMapper.toDTO(
+                ResponseDTO response = this.responseMapper.toDTO(
                         400,
                         e.getMessage(),
                         null
@@ -61,11 +56,13 @@ public class CreateUserController {
                 return ResponseEntity.status(400).body(response);
             }
 
-            ResponseDTO response = ResponseMapper.toDTO(
+            ResponseDTO response = this.responseMapper.toDTO(
                     500,
                     "Erro Interno",
                     null
             );
+
+            System.out.println(e.getMessage());
 
             return ResponseEntity.status(500).body(response);
         }
