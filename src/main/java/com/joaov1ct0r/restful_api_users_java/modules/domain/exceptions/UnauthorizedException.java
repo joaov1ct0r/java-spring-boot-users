2a78d194cb300a5b822935bb95eb715c461b4fb0
joaov1ct0r <jov9051@outlook.com>
2024-06-20 03:55:10 -0300

    feat: UnauthorizedException

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/domain/exceptions/UnauthorizedException.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/domain/exceptions/UnauthorizedException.java
@@ -1,2 +1,7 @@
-package com.joaov1ct0r.restful_api_users_java.modules.domain.exceptions;public class UnauthorizedException {
+package com.joaov1ct0r.restful_api_users_java.modules.domain.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException (String message) {
+        super(message);
+    }
 }
