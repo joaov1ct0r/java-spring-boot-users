1f82d42834ef19e568011cc624085b4861612e9a
joaov1ct0r <jov9051@outlook.com>
2024-06-24 05:59:28 -0300

    feat: CreateJWTTokenServiceDTO

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/auth/dtos/CreateJWTTokenServiceDTO.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/auth/dtos/CreateJWTTokenServiceDTO.java
@@ -1,2 +1,34 @@
-package com.joaov1ct0r.restful_api_users_java.modules.auth.dtos;public class CreateJWTTokenServiceDTO {
+package com.joaov1ct0r.restful_api_users_java.modules.auth.dtos;
+
+public class CreateJWTTokenServiceDTO {
+    private String token;
+    private CreateJWTTokenServicePayloadDTO payload;
+
+    public CreateJWTTokenServiceDTO () {}
+
+    public CreateJWTTokenServiceDTO(
+            String token,
+            CreateJWTTokenServicePayloadDTO payload
+    ) {
+        this.token = token;
+        this.payload = payload;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public CreateJWTTokenServicePayloadDTO getPayload() {
+        return this.payload;
+    }
+
+    public void setPayload(
+            CreateJWTTokenServicePayloadDTO payload
+    ) {
+        this.token = token;
+    }
 }
