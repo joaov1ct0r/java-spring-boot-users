e3989fccbbfcc9c3772924e6355cce08d43a4cfd
joaov1ct0r <jov9051@outlook.com>
2024-06-27 05:02:55 -0300

    feat: CreateCookieService

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/auth/services/CreateCookieService.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/auth/services/CreateCookieService.java
@@ -1,23 +1,19 @@
 package com.joaov1ct0r.restful_api_users_java.modules.auth.services;
 
-import org.springframework.boot.web.server.Cookie;
+import jakarta.servlet.http.Cookie;
 import org.springframework.stereotype.Service;
 import java.time.Duration;
 
 @Service
 public class CreateCookieService {
-    public Cookie execute(String cookieName, String domain) {
-        var cookie = new Cookie();
+    public Cookie execute(String cookieName, String cookieValue, String domain) {
+        var cookie = new Cookie(cookieName, cookieValue);
         cookie.setDomain(domain);
         cookie.setHttpOnly(true);
-        cookie.setName(cookieName);
         cookie.setPath("/");
         cookie.setSecure(true);
-        cookie.setSameSite(Cookie.SameSite.STRICT);
-        cookie.setMaxAge(Duration.ofHours(1));
+        cookie.setMaxAge((int) Duration.ofHours(1).toSeconds());
 
         return cookie;
-        // add response.addCookie(cookie) to the controller
-        // to filter the incoming requests and get the cookies watch: https://www.youtube.com/watch?v=-_67Cuu_5T8
     }
 }
