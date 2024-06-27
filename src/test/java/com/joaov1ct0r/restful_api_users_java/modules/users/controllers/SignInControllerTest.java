eb2f2c2560bd83fc4aad4e2671976f2c721870ad
joaov1ct0r <jov9051@outlook.com>
2024-06-27 05:02:28 -0300

    test: SignInController [e2e]

--- src/test/java/com/joaov1ct0r/restful_api_users_java/modules/users/controllers/SignInControllerTest.java
+++ src/test/java/com/joaov1ct0r/restful_api_users_java/modules/users/controllers/SignInControllerTest.java
@@ -1,2 +1,69 @@
-package com.joaov1ct0r.restful_api_users_java.modules.users.controllers;public class SignInControllerTest {
+package com.joaov1ct0r.restful_api_users_java.modules.users.controllers;
+
+import com.github.javafaker.Faker;
+import com.joaov1ct0r.restful_api_users_java.modules.auth.dtos.SignInDTO;
+import com.joaov1ct0r.restful_api_users_java.modules.domain.dtos.ResponseDTO;
+import com.joaov1ct0r.restful_api_users_java.modules.users.dtos.CreateUserDTO;
+import com.joaov1ct0r.restful_api_users_java.modules.users.utils.TestUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class SignInControllerTest {
+    private MockMvc mvc;
+
+    private Faker faker;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+
+        faker = new Faker();
+    }
+
+    @Test
+    public void shouldBeAbleToSignIn() throws Exception {
+        var createUserDTO = new CreateUserDTO(
+                faker.name().username(),
+               faker.internet().emailAddress(),
+                faker.name().firstName(),
+                faker.internet().password()
+        );
+        mvc.perform(
+                MockMvcRequestBuilders.post("/signup/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(TestUtils.objectToJson(createUserDTO))
+        ).andReturn();
+        var signInDTO = new SignInDTO(createUserDTO.getUsername(), createUserDTO.getPassword());
+
+        String signInResponse = mvc.perform(
+                MockMvcRequestBuilders.post("/signin/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(TestUtils.objectToJson(signInDTO))
+        ).andReturn().getResponse().getContentAsString();
+       ResponseDTO response = TestUtils.jsonToObject(signInResponse, ResponseDTO.class);
+
+       System.out.println(response.getMessage());
+
+        assertThat(response.getStatusCode()).isEqualTo(200);
+    }
 }
