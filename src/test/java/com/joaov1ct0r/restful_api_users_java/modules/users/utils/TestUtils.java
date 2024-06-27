4aced95788f01854255a3e2ae1c0c15c1ab5c19a
joaov1ct0r <jov9051@outlook.com>
2024-06-27 05:04:47 -0300

    feat: added json deserialization method to TestUtils

--- src/test/java/com/joaov1ct0r/restful_api_users_java/modules/users/utils/TestUtils.java
+++ src/test/java/com/joaov1ct0r/restful_api_users_java/modules/users/utils/TestUtils.java
@@ -1,7 +1,10 @@
 package com.joaov1ct0r.restful_api_users_java.modules.users.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.joaov1ct0r.restful_api_users_java.modules.domain.dtos.ResponseDTO;
 
 public class TestUtils {
     public static String objectToJson(Object obj) {
@@ -9,6 +12,16 @@ public class TestUtils {
             final ObjectMapper objMapper = new ObjectMapper();
             objMapper.registerModule(new JavaTimeModule());
             return objMapper.writeValueAsString(obj);
+        } catch(Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static ResponseDTO jsonToObject(String json, Class<ResponseDTO> responseDTO) {
+        try {
+            final ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.readValue(json, responseDTO);
         } catch(Exception e) {
             throw new RuntimeException(e.getMessage());
         }
