e5edb3263e5f69d7a7c4cbae21d299748e846293
joaov1ct0r <jov9051@outlook.com>
2024-07-19 19:23:59 -0300

    feat: AuthorizationService

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/auth/services/AuthorizationService.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/auth/services/AuthorizationService.java
@@ -1,2 +1,26 @@
-package com.joaov1ct0r.restful_api_users_java.modules.auth.services;public class AuthorizationService {
+package com.joaov1ct0r.restful_api_users_java.modules.auth.services;
+
+import com.joaov1ct0r.restful_api_users_java.modules.domain.services.BaseService;
+import com.joaov1ct0r.restful_api_users_java.modules.users.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService extends BaseService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = this.userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            throw this.unauthorizedException("Não autorizado!");
+        }
+
+        return user.get();
+    }
 }
