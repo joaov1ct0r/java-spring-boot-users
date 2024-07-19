435ec2f04697579f0ae3683ffa390bd710eaeecb
joaov1ct0r <jov9051@outlook.com>
2024-07-19 19:04:11 -0300

    feat: UserRepository

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/repositories/UserRepository.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.joaov1ct0r.restful_api_users_java.modules.users.repositories;
 
 import com.joaov1ct0r.restful_api_users_java.modules.users.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 import java.util.UUID;
 
