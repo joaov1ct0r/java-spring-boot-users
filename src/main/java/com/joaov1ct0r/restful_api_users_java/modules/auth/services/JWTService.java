639a124471d99035c6ae957b1e125d39667228a8
joaov1ct0r <jov9051@outlook.com>
2024-07-19 19:10:20 -0300

    feat: JWTService

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/auth/services/JWTService.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/auth/services/JWTService.java
@@ -1,2 +1,26 @@
-package com.joaov1ct0r.restful_api_users_java.modules.auth.services;public class JWTService {
+package com.joaov1ct0r.restful_api_users_java.modules.auth.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+    @Value("${security.token.secret}")
+    private String secret;
+
+    public DecodedJWT decodeToken(String token) {
+        Algorithm algo = Algorithm.HMAC256(this.secret);
+
+        try {
+            return JWT
+                    .require(algo)
+                    .build()
+                    .verify(token);
+        } catch (Exception e) {
+            throw new RuntimeException("Internal Server Error");
+        }
+    }
 }
