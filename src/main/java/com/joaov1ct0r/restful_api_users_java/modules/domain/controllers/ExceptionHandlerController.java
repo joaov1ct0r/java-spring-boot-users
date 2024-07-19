49251450826ac2191fd830b7af635b584267f736
joaov1ct0r <jov9051@outlook.com>
2024-07-19 19:07:20 -0300

    feat: ExceptionHandlerController added

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/domain/controllers/ExceptionHandlerController.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/domain/controllers/ExceptionHandlerController.java
@@ -2,9 +2,12 @@ package com.joaov1ct0r.restful_api_users_java.modules.domain.controllers;
 
 import com.joaov1ct0r.restful_api_users_java.modules.domain.dtos.ResponseDTO;
 import com.joaov1ct0r.restful_api_users_java.modules.domain.exceptions.BadRequestException;
+import com.joaov1ct0r.restful_api_users_java.modules.domain.exceptions.UnauthorizedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Arrays;
 
 @ControllerAdvice
 public class ExceptionHandlerController extends BaseController {
@@ -20,9 +23,19 @@ public class ExceptionHandlerController extends BaseController {
                 return ResponseEntity.status(400).body(response);
        }
 
+       if (e instanceof UnauthorizedException) {
+           ResponseDTO response = this.responseMapper.toDTO(
+                   403,
+                   e.getMessage(),
+                   null
+           );
+
+           return ResponseEntity.status(403).body(response);
+       }
+
        ResponseDTO response = this.responseMapper.toDTO(
                500,
-               "Erro interno",
+               Arrays.toString(e.getStackTrace()),
                null
        );
 
