4d54b392c7ea2aa1342f6f93e061617ca6853e85
joaov1ct0r <jov9051@outlook.com>
2024-07-20 19:10:37 -0300

    feat: ForbiddenException

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/domain/exceptions/ForbiddenException.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/domain/exceptions/ForbiddenException.java
@@ -1,2 +1,7 @@
-package com.joaov1ct0r.restful_api_users_java.modules.domain.exceptions;public class ForbiddenException {
+package com.joaov1ct0r.restful_api_users_java.modules.domain.exceptions;
+
+public class ForbiddenException extends RuntimeException{
+    public ForbiddenException(String message) {
+        super(message);
+    }
 }
