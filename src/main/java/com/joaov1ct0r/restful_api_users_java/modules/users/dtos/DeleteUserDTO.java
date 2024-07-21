b8a86ba2e2060ab10c5f089941beb67537044c06
joaov1ct0r <jov9051@outlook.com>
2024-07-21 20:31:41 -0300

    feat: DeleteUserDTO

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/dtos/DeleteUserDTO.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/dtos/DeleteUserDTO.java
@@ -1,2 +1,21 @@
-package com.joaov1ct0r.restful_api_users_java.modules.users.dtos;public class DeleteUserDTO {
+package com.joaov1ct0r.restful_api_users_java.modules.users.dtos;
+
+public class DeleteUserDTO {
+    private String userId;
+
+    public DeleteUserDTO() {}
+
+    public DeleteUserDTO(
+            String userId
+    ) {
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
