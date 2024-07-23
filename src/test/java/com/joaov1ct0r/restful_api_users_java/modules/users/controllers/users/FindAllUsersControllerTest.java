8deed9e3d2a5927fa6eac441f472d4e13001e3fd
joaov1ct0r <jov9051@outlook.com>
2024-07-23 11:40:15 -0300

    test: FindAllUsersController [e2e]

--- src/test/java/com/joaov1ct0r/restful_api_users_java/modules/users/controllers/users/FindAllUsersControllerTest.java
+++ src/test/java/com/joaov1ct0r/restful_api_users_java/modules/users/controllers/users/FindAllUsersControllerTest.java
@@ -1,2 +1,90 @@
-package com.joaov1ct0r.restful_api_users_java.modules.users.controllers.users;public class FindAllUsersControllerTest {
+package com.joaov1ct0r.restful_api_users_java.modules.users.controllers.users;
+
+import com.github.javafaker.Faker;
+import com.joaov1ct0r.restful_api_users_java.modules.auth.dtos.SignInDTO;
+import com.joaov1ct0r.restful_api_users_java.modules.users.dtos.CreateUserDTO;
+import com.joaov1ct0r.restful_api_users_java.modules.users.utils.TestUtils;
+import jakarta.servlet.http.Cookie;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.joaov1ct0r.restful_api_users_java.modules.users.dtos.FindAllUsersDTO;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class FindAllUsersControllerTest {
+    private MockMvc mvc;
+
+    private Faker faker;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+
+        faker = new Faker();
+    }
+
+    @Test
+    public void shouldBeAbleToFindAllUsers() throws Exception {
+        var createUserDTO = new CreateUserDTO(
+                faker.name().username(),
+                faker.internet().emailAddress(),
+                faker.name().firstName(),
+                faker.internet().password()
+        );
+        mvc.perform(
+                MockMvcRequestBuilders.post("/signup/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(TestUtils.objectToJson(createUserDTO))
+        ).andReturn().getResponse();
+
+        var signInDTO = new SignInDTO(
+                createUserDTO.getUsername(),
+                createUserDTO.getPassword()
+        );
+        Cookie cookieAuthorization = mvc.perform(
+                MockMvcRequestBuilders.post("/signin/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(TestUtils.objectToJson(signInDTO))
+        ).andReturn().getResponse().getCookie("authorization");
+
+        assert cookieAuthorization != null;
+
+        var findAllUsersDTO = new FindAllUsersDTO(
+                20,
+                1,
+                "any_name",
+                "any_username",
+                "any_email@mail.com"
+        );
+
+        var findAllUsersResponse = mvc.perform(
+                MockMvcRequestBuilders.get("/user/")
+                        .cookie(cookieAuthorization)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(TestUtils.objectToJson(findAllUsersDTO))
+        ).andReturn().getResponse();
+
+        int findAllUsersResponseStatusCode = findAllUsersResponse.getStatus();
+
+        assertThat(findAllUsersResponseStatusCode).isEqualTo(200);
+
+    }
 }
