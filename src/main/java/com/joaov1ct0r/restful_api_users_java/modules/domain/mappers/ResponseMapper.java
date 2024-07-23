8e26dae522fe906ebb33dc415c77abe7c74b37f4
joaov1ct0r <jov9051@outlook.com>
2024-07-23 11:37:00 -0300

    feat: added ResponsePagDTO to ResponseMapper

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/domain/mappers/ResponseMapper.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/domain/mappers/ResponseMapper.java
@@ -1,11 +1,30 @@
 package com.joaov1ct0r.restful_api_users_java.modules.domain.mappers;
 
 import com.joaov1ct0r.restful_api_users_java.modules.domain.dtos.ResponseDTO;
+import com.joaov1ct0r.restful_api_users_java.modules.domain.dtos.ResponsePagDTO;
 import jakarta.annotation.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ResponseMapper {
+    public ResponsePagDTO toPagDTO(
+            int statusCode,
+            String message,
+            @Nullable Object resource,
+            int prevPage,
+            int nextPage,
+            int total
+    ) {
+        return new ResponsePagDTO(
+                statusCode,
+                message,
+                resource,
+                prevPage,
+                nextPage,
+                total
+        );
+    }
+
     public ResponseDTO toDTO(
             Number statusCode,
             String message,
