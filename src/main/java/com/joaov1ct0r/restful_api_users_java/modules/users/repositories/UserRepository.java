914d05c9039379a59863f0c60e37e2b75b4d6446
joaov1ct0r <jov9051@outlook.com>
2024-07-23 11:37:26 -0300

    feat: added methods count and find all by criteria

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/repositories/UserRepository.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/repositories/UserRepository.java
@@ -1,11 +1,25 @@
 package com.joaov1ct0r.restful_api_users_java.modules.users.repositories;
 
 import com.joaov1ct0r.restful_api_users_java.modules.users.entities.UserEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Pageable;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByUsername(String username);
     Optional<UserEntity> findByEmail(String email);
+    Page<UserEntity> countByNameContainingAndUsernameContainingAndEmailContaining(
+            String name,
+            String username,
+            String email,
+            Pageable pageable
+    );
+    Page<UserEntity> findAllByNameContainingAndUsernameContainingAndEmailContaining(
+            String name,
+            String username,
+            String email,
+            Pageable pageable
+    );
 }
