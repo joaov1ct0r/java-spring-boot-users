76763a77120c68a61ed926946858953671a51800
joaov1ct0r <jov9051@outlook.com>
2024-07-23 11:37:53 -0300

    feat: CountAllUsersDTO

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/dtos/CountAllUsersDTO.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/users/dtos/CountAllUsersDTO.java
@@ -1,2 +1,68 @@
-package com.joaov1ct0r.restful_api_users_java.modules.users.dtos;public class CountAllUsersDTO {
+package com.joaov1ct0r.restful_api_users_java.modules.users.dtos;
+
+import jakarta.annotation.Nullable;
+
+public class CountAllUsersDTO {
+    private int perPage;
+    private int page;
+    private @Nullable String name;
+    private @Nullable String username;
+    private @Nullable String email;
+
+    public CountAllUsersDTO() {}
+
+    public CountAllUsersDTO(
+            int perPage,
+            int page,
+            @Nullable String name,
+            @Nullable String username,
+            @Nullable String email
+    ) {
+        this.perPage = perPage;
+        this.page = page;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getPerPage() {
+        return this.perPage;
+    }
+
+    public void setPerPage(int perPage) {
+        this.perPage = perPage;
+    }
+
+    public int getPage() {
+        return this.page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public @Nullable String getName() {
+        return this.name;
+    }
+
+    public void setName(@Nullable String name) {
+        this.name = name;
+    }
+
+    public @Nullable String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(@Nullable String username) {
+        this.username = username;
+    }
+
+    public @Nullable String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(@Nullable String email) {
+        this.email = email;
+    }
 }
+
