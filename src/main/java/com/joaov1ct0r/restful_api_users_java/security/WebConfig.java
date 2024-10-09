9d70bc7a9f4de64f831b44b11151dd192ae204e2
joaov1ct0r <jov9051@outlook.com>
2024-10-09 17:23:48 -0300

    feat: added cors config

---
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/security/WebConfig.java
@@ -0,0 +1,18 @@
+package com.joaov1ct0r.restful_api_users_java.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply CORS to all paths
+                .allowedOrigins("http://localhost:3000") // Specify allowed origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed HTTP methods
+                .allowedHeaders("*") // Specify allowed headers
+                .allowCredentials(true); // Allow credentials (cookies, authorization headers)
+    }
+}
