76a5ad39d129bd35435184e71e7c8fa4977e1f23
joaov1ct0r <jov9051@outlook.com>
2024-11-03 14:14:42 -0300

    refactor: added cookies configs httpOnly and secure to false

--- src/main/java/com/joaov1ct0r/restful_api_users_java/modules/auth/services/CreateCookieService.java
+++ src/main/java/com/joaov1ct0r/restful_api_users_java/modules/auth/services/CreateCookieService.java
@@ -10,9 +10,11 @@ public class CreateCookieService {
         int twoDaysInSeconds = 2 * 24 * 60 * 60; // 2 dias em segundos
         var cookie = new Cookie(cookieName, cookieValue);
         cookie.setDomain(domain);
-        cookie.setHttpOnly(true);
+        cookie.setHttpOnly(false);
+//        cookie.setHttpOnly(true);
         cookie.setPath("/");
-        cookie.setSecure(true);
+        cookie.setSecure(false);
+//        cookie.setSecure(true);
         cookie.setMaxAge(twoDaysInSeconds);
 
         return cookie;
